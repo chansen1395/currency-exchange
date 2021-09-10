a0bd9c2989d830bf9b95fb97815becb7f2a0376e
Connor Hansen <chansen13@georgefox.edu>
2021-09-10 10:25:21 -0700

    add multiple currencies in a list, to-do: generate selection with all currencies.

--- src/exchange.js
+++ src/exchange.js
@@ -1,4 +1,4 @@
-export default class MarsCam {  
+export default class ExchangeRate {  
   static getRate(currencyType) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
@@ -14,4 +14,20 @@ export default class MarsCam {
       request.send();
     });
   }
+
+  // static getRate(currencyType) {
+  //   return new Promise(function(resolve, reject) {
+  //     let request = new XMLHttpRequest();
+  //     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyType}`;
+  //     request.onload = function() {
+  //       if (this.status === 200) {
+  //         resolve(request.response);
+  //       } else {
+  //         reject(request.response);
+  //       }
+  //     };
+  //     request.open("GET", url, true);
+  //     request.send();
+  //   });
+  // }
 }
--- src/index.js
+++ src/index.js
@@ -4,29 +4,50 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRate from './exchange.js';
 
-function clearFields() {
-  $('#exchange').val("");
-  // $('.showAPOD').text("");
-  // $('.showMarsCam').text("");
-  // $('.showDONKI').text("");
-}
+// function clearFields() {
+//   $('#exchange').val("");
+//   // $('.showAPOD').text("");
+//   // $('.showMarsCam').text("");
+//   // $('.showDONKI').text("");
+// }
 
-$(document).ready(function() {
-  $('#exchange').click(function() {
+
+$(document).ready(function () {
+  // let promise = ExchangeRate.getCurrencies();
+  // promise.then(function (response) {
+  //   const body = JSON.parse(response);
+  //   $(".showRate").append(`<p>Current exchange rate for $1.00 ` + body.base_code + ` is ` + formatter.format(body.conversion_rate.toFixed(2)).replace('$', '') + ` ` + body.target_code + `</p>`);
+  //   $(".showAmount").append(`<p>Exchange rate for ${formatter.format(currencyAmount)} ` + body.base_code + ` is ` + formatter.format(body.conversion_rate.toFixed(2) * currencyAmount).replace('$', '') + ` ` + body.target_code + `</p>`);
+  // }, function (error) {
+  //   $('.showErrors').text(`There was an error processing your request: ${error} Unsupported currency type: ${currencyType}`);
+  // });
+  $('#exchange').click(function () {
     let currencyType = $('#toExchange').val();
     let currencyAmount = $('#amount').val();
-    clearFields();
-    $('.showErrors').empty();
-    
+    var formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    // try {
+    //   ExchangeRate.getRate(currencyType);
+    // } catch (error) {
+    //   $('.showErrors').text(error);
+    //   // expected output: ReferenceError: nonExistentFunction is not defined
+    //   // Note - error messages will vary depending on browser
+    // }
+
+    // clearFields();
+
+    $('.showErrors, .showRate, .showAmount').empty();
     let promise = ExchangeRate.getRate(currencyType);
-    promise.then(function(response) {
+    promise.then(function (response) {
       const body = JSON.parse(response);
-      $(".showRate").append(`<p>Current exchange rate for 1 ` + body.base_code + ` is ` + body.conversion_rate.toFixed(2) + ` ` + body.target_code + `</p>`);
-      $(".showAmount").append(`<p>Exchange rate for ${currencyAmount} ` + body.base_code + ` is ` + (body.conversion_rate.toFixed(2) * currencyAmount) + ` ` + body.target_code + `</p>`);
-    }, function(error) {
+      $(".showRate").append(`<p>Current exchange rate for $1.00 ` + body.base_code + ` is ` + formatter.format(body.conversion_rate.toFixed(2)).replace('$', '') + ` ` + body.target_code + `</p>`);
+      $(".showAmount").append(`<p>Exchange rate for ${formatter.format(currencyAmount)} ` + body.base_code + ` is ` + formatter.format(body.conversion_rate.toFixed(2) * currencyAmount).replace('$', '') + ` ` + body.target_code + `</p>`);
+    }, function (error) {
       $('.showErrors').text(`There was an error processing your request: ${error} Unsupported currency type: ${currencyType}`);
     });
-    
+
     // let promise1 = MarsCam.getAPOD(earth_date);
     // promise1.then(function(response) {
     //   const body = JSON.parse(response);
