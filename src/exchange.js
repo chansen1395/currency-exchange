cc135bff5af8236aaae0ad3a2603f96e0a9386cf
Connor Hansen <chansen13@georgefox.edu>
2021-09-10 12:00:56 -0700

    WIP formatting and reference list for conversion codes for user.

--- src/exchange.js
+++ src/exchange.js
@@ -1,8 +1,8 @@
 export default class ExchangeRate {  
-  static getRate(currencyType, currencyAmount) {
+  static getRate(currencyFrom, currencyTo, currencyAmount) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyType}/${currencyAmount}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}/${currencyAmount}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/index.js
+++ src/index.js
@@ -4,48 +4,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRate from './exchange.js';
 
-// function clearFields() {
-//   $('#exchange').val("");
-//   // $('.showAPOD').text("");
-//   // $('.showMarsCam').text("");
-//   // $('.showDONKI').text("");
-// }
-
-
 $(document).ready(function () {
+
+  // **************************************************
+  // Generate dropdown of all currencies to exchange to
+  // **************************************************
   let promise = ExchangeRate.getCurrencies();
   promise.then(function (response) {
     const body = JSON.parse(response);
     var currList = Object.keys(body.conversion_rates);
     for (var i in currList) {
-      $('#toExchange').append(`<option value="` + currList[i] + `">${currList[i]}</option>`);
+      $('#exchangeFrom').append(`<option value="` + currList[i] + `">${currList[i]}</option>`);
+      $('#exchangeTo').append(`<option value="` + currList[i] + `">${currList[i]}</option>`);
     }
   }, function (error) {
     $('.showErrors').text(`There was an error processing your request: ${error}`);
   });
 
-
-
+  // *********************************************
+  // Use API calls to generate output for the user
+  // *********************************************
   $('#exchange').click(function () {
-    let currencyType = $('#toExchange').val();
+    $('.showErrors, .showRate, .showAmount').empty();
+    $('#results').show();
+    let currencyFrom = $('#exchangeFrom').val();
+    let currencyTo = $('#exchangeTo').val();
     let currencyAmount = $('#amount').val();
+
+    // Formatter to convert output to xx,xxx.xx
     var formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
     });
-    // try {
-    //   ExchangeRate.getRate(currencyType);
-    // } catch (error) {
-    //   $('.showErrors').text(error);
-    //   // expected output: ReferenceError: nonExistentFunction is not defined
-    //   // Note - error messages will vary depending on browser
-    // }
 
-    // clearFields();
-
-    $('.showErrors, .showRate, .showAmount').empty();
-
-    let promise = ExchangeRate.getRate(currencyType, currencyAmount);
+    // Formatted output to user
+    let promise = ExchangeRate.getRate(currencyFrom, currencyTo, currencyAmount);
     promise.then(function (response) {
       const body = JSON.parse(response);
       try {
@@ -55,29 +48,7 @@ $(document).ready(function () {
         $(".showErrors").text(`Error: Please input a number to convert. ${error.message}`);
       }
     }, function (error) {
-      $('.showErrors').text(`There was an error processing your request: ${error} Unsupported currency type: ${currencyType}`);
+      $('.showErrors').text(`There was an error processing your request: ${error} Unsupported currency type: ${currencyTo}`);
     });
-
-    // let promise1 = MarsCam.getAPOD(earth_date);
-    // promise1.then(function(response) {
-    //   const body = JSON.parse(response);
-    //   $(".showAPOD").append(`<img src='` + body.hdurl + `'style='height:600px;'/><br>
-    //   <p class="card">Explanation: ` + body.explanation + `</p>`);
-    // });
-
-    // let promise2 = MarsCam.getDONKI();
-    // promise2.then(function(response) {
-    //   const body = JSON.parse(response);
-    //   let i = 0;
-    //   if (body.length > 3) {
-    //     i = body.length - 3;
-    //   }
-    //   while (i < body.length) {
-    //     $(".showDONKI").append(`<h5>Flare Name: ` + body[i].instruments[0].displayName + `</h5>`);
-    //     $(".showDONKI").append(`<li>Peak Time: ` + body[i].peakTime + `</li>`);
-    //     $(".showDONKI").append(`<li>Flare Class: ` + body[i].classType + `</li>`);
-    //     i++;
-    //   }
-    // });
   });
 });
